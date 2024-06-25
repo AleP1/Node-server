06b5cb30ee546fa68b42777b1b0642b5d5f85d32
AleP1 <alejo.k26@gmail.com>
2024-06-24 23:48:20 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const {envs} = require('./config/env')
+const {startServer} = require('./server/server')
+
+
+const main = () => {
+ startServer({
+    port: envs.PORT,
+    publicPath: envs.PUBLIC_PATH
+ })
+}
+
+
+(async () => {
+    await main()
+})()
+
+    
